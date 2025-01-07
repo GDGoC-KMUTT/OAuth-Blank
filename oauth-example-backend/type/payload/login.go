d0aa9af0874128ab91f4f34d2bd164859994ff1d
Sunthewhat <chotiwet.wisi@icloud.com>
2025-01-07 11:37:44 +0700

    remove backend

--- oauth-example-backend/type/payload/login.go
+++
@@ -1,5 +0,0 @@
-package payload
-
-type OauthCallback struct {
-	Code *string `json:"code" validate:"required"`
-}
--- oauth-example-backend/type/payload/profile.go
+++
@@ -1,9 +0,0 @@
-package payload
-
-type Profile struct {
-	Id        *uint64 `json:"id"`
-	Oid       *string `json:"oid"`
-	Firstname *string `json:"firstname"`
-	Lastname  *string `json:"lastname"`
-	PhotoUrl  *string `json:"photoUrl"`
-}
